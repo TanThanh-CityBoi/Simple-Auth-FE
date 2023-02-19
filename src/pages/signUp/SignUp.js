b7296a2d824c43ab3e58649106bfc25965844fe7
tanthanh-cityboi <tanthanhe@gmail.com>
2023-02-19 12:09:22 +0700

    css

--- src/pages/signUp/SignUp.js
+++ src/pages/signUp/SignUp.js
@@ -14,7 +14,7 @@ function SignUp() {
     }
     return (
         <div className="sign-up-wrapper d-flex justify-content-center">
-            <div className="sign-up-form ">
+            <div className="sign-up-form mt-5 mb-5">
                 <h1 className='text-center font-weight-bold mb-5'>Sign Up</h1>
                 <SignUpForm onSubmit={handleSignUp} />
                 <Link to="/sign-in" className='btn sign-in-btn mt-2 text-center'>
